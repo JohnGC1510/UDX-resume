acce6ad254ba1bdedccee0fd3d08d9f410ab1b7f
JohnGC1510 <johngc1510@gmail.com>
2020-08-03 16:38:39 +0000

    added functionallity to allow the form to alert me when it has been completed

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -31,6 +31,8 @@ function repoInformationHTML(repos){
 }
 
 function fetchGitHubInformation(event) {
+   $("#gh-user-data").html(""); 
+   $("#gh-repo-data").html(""); 
   var username = $("#gh-username").val();
   if (!username) {
     $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -53,13 +55,18 @@ function fetchGitHubInformation(event) {
     function (errorResponse) {
       if (errorResponse.status === 404) {
         $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
-      } else {
+      }  else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
+      }   else{
           console.log(errorResponse);
           $("#gh-user-data").html(`<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
       }
     }
   );
 }
+
+$(document).ready(fetchGitHubInformation);
 
 /* A promise in coding is when an event occurs dependent on another event finishing - syntax:
  .when('We have a respone from GitHub API').then(function to display it in the gh-user-data div with capability to handle errors)*/
